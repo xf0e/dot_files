292765f9b076c2b1cac2376822993ef8ccf7c7c0
xf0e <xf0e@users.noreply.github.com>
2016-06-30 12:10:23 +0200

    Create bash_json_value.sh

---
+++ bash_json_value.sh
@@ -0,0 +1,9 @@
+#get json value from a variable
+function jsonval {
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
+json=$(curl -s $link)
+prop=$prop_name
+TMP=`jsonval`
